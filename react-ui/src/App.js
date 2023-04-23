0249b6f22b58b8f9196a35724eb13673128b1956
Silyan99 <bsilyan99@gmail.com>
2023-04-23 11:59:59 -0400

    Code updated

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,25 +6,6 @@ import PredictForm from "./componenets/PrdictForm";
 // import "./App.css";
 //
 function App() {
-  // const [data, setData] = useState({});
-  // const [showLoading, setShowLoading] = useState(false);
-  // const apiUrl = "http://localhost:3000/run";
-  // //runs once after the first rendering of page
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     axios
-  //       .get(apiUrl)
-  //       .then((result) => {
-  //         console.log("result.data:", result.data);
-  //         setData(result.data);
-  //         setShowLoading(false);
-  //       })
-  //       .catch((error) => {
-  //         console.log("error in fetchData:", error);
-  //       });
-  //   };
-  //   fetchData();
-  // }, []);
 
   return (
     <div>
